21abb3ace2ce8a7cbdc5615b5382136a82d455dd
pranavparmaj <pranavparmaj209@gmail.com>
2026-03-09 19:01:43 +0530

    Step 8: Implemented snippet retrieval engine using FAISS

--- backend/rag_engine.py
+++ backend/rag_engine.py
@@ -1,5 +1,8 @@
 import json
 import os
+import numpy as np
+from embedding_engine import generate_embedding
+from vector_store import load_faiss_index
 
 
 def load_snippet_metadata():
@@ -41,3 +44,22 @@ def get_snippets():
         })
 
     return snippets
+
+def retrieve_relevant_snippets(query, top_k=2):
+
+    index = load_faiss_index()
+
+    snippets = get_snippets()
+
+    query_vector = generate_embedding(query)
+
+    query_vector = np.array([query_vector]).astype("float32")
+
+    distances, indices = index.search(query_vector, top_k)
+
+    retrieved = []
+
+    for i in indices[0]:
+        retrieved.append(snippets[i])
+
+    return retrieved
