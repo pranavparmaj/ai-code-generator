75836d49946baa2e7650a37b584efe6468131cd8
pranavparmaj <pranavparmaj209@gmail.com>
2026-03-09 17:36:19 +0530

    Step 7: Implemented FAISS vector database for snippet embeddings

---
+++ backend/vector_store.py
@@ -0,0 +1,37 @@
+import faiss
+import numpy as np
+import os
+
+
+INDEX_PATH = os.path.abspath("../vector_db/snippet_index.faiss")
+
+
+def create_faiss_index(embedded_snippets):
+
+    dimension = len(embedded_snippets[0]["embedding"])
+
+    index = faiss.IndexFlatL2(dimension)
+
+    vectors = []
+
+    for snippet in embedded_snippets:
+        vectors.append(snippet["embedding"])
+
+    vectors = np.array(vectors).astype("float32")
+
+    index.add(vectors)
+
+    faiss.write_index(index, INDEX_PATH)
+
+    return index
+
+
+def load_faiss_index():
+
+    if os.path.exists(INDEX_PATH):
+
+        index = faiss.read_index(INDEX_PATH)
+
+        return index
+
+    return None
